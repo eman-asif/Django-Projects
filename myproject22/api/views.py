733036a0c3fbe460ac89c9c74d4f09f715354739
Eman Asif <136709185+eman-asif@users.noreply.github.com>
2025-07-17 19:59:22 +0500

    myproject 22, 23, 24 added

---
+++ myproject22/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import NewStudent
+from .serializers import NewStudentSerializer
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated , IsAuthenticatedOrReadOnly
+from rest_framework.throttling import AnonRateThrottle , UserRateThrottle 
+from rest_framework.generics import ListAPIView
+# from .throttling import EmanRateThrottle
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+# from .custompermissions import MyPermission
+
+
+class StudentList(ListAPIView):
+    queryset = NewStudent.objects.all()
+    serializer_class = NewStudentSerializer
+    filterset_fields = ['passby']
